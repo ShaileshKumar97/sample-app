3d672efaf0ff29e2d22e90f5831b8e7ad3a9f84d
shaileshkumar97 <kumar.shailesh1597@gmail.com>
2022-11-04 10:57:55 +0000

    docs: Updated the description.

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import os
 
 """
 # Welcome to Streamlit!
+
+This is a Sample Streamlit App.
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
