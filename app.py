4806c8b6766d27a750657eccd16925c1ae1c323b
shaileshkumar97 <kumar.shailesh1597@gmail.com>
2022-11-04 10:32:45 +0000

    fix: Remove the routes

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import pandas as pd
 import streamlit as st
 import os
 
-routes = os.environ["ROUTE"]
-
 """
 # Welcome to Streamlit!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
